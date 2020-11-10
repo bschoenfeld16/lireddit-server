55641c4cea3b418e87f58b2e3c0672d2cec2d0bc
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-09 20:51:24 -0600

    setting up graphQL reading of posts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
 import {buildSchema} from "type-graphql";
 import {HelloResolver} from "./resolvers/hello";
+import {PostResolver} from "./resolvers/post";
+import "reflect-metadata";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -13,9 +15,10 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false
-        })
+        }),
+        context: () => ({em: orm.em})
     });
 
     apolloServer.applyMiddleware({app});
