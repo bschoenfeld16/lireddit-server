94be5c464eaa43caec8096983911ca220ba2a563
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-11 16:48:39 -0600

    simplify our types file per Ben Awad's suggestion

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,8 @@
 import {EntityManager} from "@mikro-orm/core";
 import {Request, Response} from "express";
-import {SessionData} from "express-session";
-
-declare module 'express-session' {
-    interface SessionData {
-        userId?: number;
-    }
-}
 
 export type MyContext = {
     em: EntityManager;
-    req: Request & { session: SessionData };
+    req: Request & { session: {userId?: number} };
     res: Response;
 }
