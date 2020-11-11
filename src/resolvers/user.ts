77c27a61e03fa593beb3809d0bb24633655fd75e
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-10 18:42:01 -0600

    handling user sesessions

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import {Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver} from "type-graphql";
+import {Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver} from "type-graphql";
 import {MyContext} from "../types";
 import {User} from "../entities/User";
 import argon2 from "argon2"
@@ -32,6 +32,17 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+    @Query(() => User, {nullable: true})
+    async me(
+        @Ctx() {em, req}: MyContext
+    ) {
+        if (!req.session.userId) {
+            return null
+        }
+
+        return await em.findOne(User, {id: req.session.userId});
+    }
+
     @Mutation(() => UserResponse)
     async register(
         @Arg("options") options: UsernamePasswordInput,
@@ -78,7 +89,7 @@ export class UserResolver {
     @Mutation(() => UserResponse)
     async login(
         @Arg("options") options: UsernamePasswordInput,
-        @Ctx() {em}: MyContext
+        @Ctx() {em, req}: MyContext
     ): Promise<UserResponse> {
         const user = await em.findOne(User, {username: options.username});
         if (!user) {
@@ -99,6 +110,9 @@ export class UserResolver {
             }
 
         }
+
+        req.session.userId = user.id;
+
         return {user}
     }
 
