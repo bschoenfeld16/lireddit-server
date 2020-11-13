d3ddf7d698316f483a4ee4c9a098ed51f8e01ad1
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-12 20:14:10 -0600

    logout mutation resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,6 +2,7 @@ import {Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver} from 
 import {MyContext} from "../types";
 import {User} from "../entities/User";
 import argon2 from "argon2"
+import {COOKIE_NAME} from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -116,5 +117,22 @@ export class UserResolver {
 
         return {user}
     }
+
+    @Mutation(() => Boolean)
+    logout(
+        @Ctx() {req, res}: MyContext
+    ) {
+        return new Promise(resolve =>
+            req.session.destroy(err => {
+                res.clearCookie(COOKIE_NAME);
+                if (err) {
+                    console.log(err);
+                    resolve(false);
+                    return
+                }
+                resolve(true)
+            }))
+    }
+
 
 }
