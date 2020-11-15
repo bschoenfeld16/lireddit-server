5ad404b36313a5bcabb4c513aaac9b1cfd034167
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-14 18:37:09 -0600

    adding pagination to posts query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -11,6 +11,7 @@ import {
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 import { isAuth } from "../middleware/isAuth";
+import { getConnection } from "typeorm";
 
 @InputType()
 class PostInput {
@@ -24,8 +25,23 @@ class PostInput {
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
-    posts(): Promise<Post[]> {
-        return Post.find();
+    posts(
+        @Arg("limit") limit: number,
+        @Arg("cursor", () => String, { nullable: true }) cursor: string
+    ): Promise<Post[]> {
+        const realLimit = Math.min(50, limit);
+        let qb = getConnection()
+            .getRepository(Post)
+            .createQueryBuilder("p")
+            .orderBy('"createdAt"', "DESC")
+            .take(realLimit);
+        if (cursor) {
+            qb.where('"createdAt" < :cursor', {
+                cursor: new Date(parseInt(cursor)),
+            });
+        }
+
+        return qb.getMany();
     }
 
     @Query(() => Post, { nullable: true })
