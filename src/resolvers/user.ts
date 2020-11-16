be92a4d8ba502f88bc3958c56e83a492f8d5015e
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-15 20:51:50 -0600

    field resolver for user email. Only return email for current user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,11 +2,13 @@ import {
     Arg,
     Ctx,
     Field,
+    FieldResolver,
     InputType,
     Mutation,
     ObjectType,
     Query,
     Resolver,
+    Root,
 } from "type-graphql";
 import { MyContext } from "../types";
 import { User } from "../entities/User";
@@ -46,8 +48,17 @@ class UserResponse {
     user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+    @FieldResolver(() => String)
+    email(@Root() user: User, @Ctx() { req }: MyContext) {
+        if (req.session.userId === user.id) {
+            return user.email;
+        }
+
+        return "";
+    }
+
     @Mutation(() => UserResponse)
     async changePassword(
         @Arg("token") token: string,
